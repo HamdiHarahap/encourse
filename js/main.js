eca626557fd364d0b2b4fe58178a406960e86cf9
HamdiHarahap <hamdiharahap2005@gmail.com>
2024-05-27 13:43:18 +0700

    commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 feather.replace()
+
+const menu = document.querySelector('.menu')
+const list = document.querySelector('.list')
+
+menu.addEventListener('click', function () {
+    list.classList.toggle('alive')
+})
